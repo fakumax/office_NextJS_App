d4a4c26572daedcf2fe29d47af3d88915dcf7603
Facundo Vergara <fakumax@gmail.com>
2023-01-23 00:47:56 -0300

    Testing

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,10 @@ import './globals.css';
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='es'>
+    <html
+      lang='es'
+      className='h-72 bg-gradient-to-r from-purple-600 to-blue-600'
+    >
       <body>{children}</body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <h1 className='text-3xl font-bold '>Sitio en construcción V3</h1>;
+  return (
+    <>
+      <h1 class='text-5xl p-4 text-center font-bold bg-gradient-to-r from-purple-600 via-pink-600 to-blue-600'>
+        Página en construcción
+      </h1>
+      <button class='m-4 p-1 rounded-full bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500'>
+        <span class='block text-black px-4 py-2 font-semibold rounded-full bg-white'>
+          Continuar
+        </span>
+      </button>
+    </>
+  );
 }
